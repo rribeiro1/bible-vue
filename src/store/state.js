f46a767ec0e358366e4fc1c6e74817c22a039c4e
Rafael Ribeiro <rafael.pr10@gmail.com>
2018-01-20 11:24:50 -0200

    Adicionando Funções para obter próxima passagem e passagem anterior

--- src/store/state.js
+++ src/store/state.js
@@ -1,10 +1,10 @@
 
 export default {
     passage: {
-        id: '1',
-        book: '0',
-        chapter: '1',
-        verse: '1',
+        id: 1,
+        book: 0,
+        chapter: 1,
+        verse: 1,
         passage: ''
     }
 }
